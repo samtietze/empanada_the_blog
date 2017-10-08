f6e0f25f3517aa6e95680df1414670aa6fee4a03
Sam Tietze <samtietze@gmail.com>
2017-10-08 15:52:54 -0500

    More routes for entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  get '/' => 'entries#index'
 
   get '/entries' => 'entries#index'
 
+  get '/entries/new' => 'entries#new'
+
+  post '/entries' => 'entries#create'
+
+  get '/entries/:id' => 'entries#show'
+
+  get '/entries/:id/edit' => 'entries#edit'
+
+  put '/entries/:id' => 'entries#update'
+
+  delete '/entries/:id' => 'entries#destroy'
+
+  get '/' => 'entries#index'
 
   root 'entries#index'
 end
