015d623860d98b08edcdf7440f7a80d440fde9f4
Sam Tietze <samtietze@gmail.com>
2017-10-08 23:34:17 -0500

    Initial user commit; changing location

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '~> 2.4.1'
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
@@ -28,7 +29,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = user.id
+      redirect_to user_path
+    else
+      @errors = @user.errors.full_messages
+      render 'users/new'
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :username, :email, :password)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
